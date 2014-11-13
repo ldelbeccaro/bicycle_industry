088025831fc5fff79f7a4f7a7d147e25bd0ff193
Laura Del Beccaro <lauradelbeccaro@gmail.com>
2014-11-13 00:18:45 +0000

    initial commit

---
+++ bicycle_industry.py
@@ -0,0 +1,44 @@
+# Bicycle Industry Project
+import sys
+import random
+try:
+    from bike_classes import *
+except ImportError:
+    print 'Error importing file.'
+
+yellow = Bike('Cruiser', 4, 150)
+red = Bike('Mountain', 10, 400)
+blue = Bike('Rider', 6, 200)
+green = Bike('Fixie', 3, 75)
+black = Bike('Intense', 8, 600)
+white = Bike('Tricycle', 4, 100)
+
+mikeshop = BikeShop('Mikes Bikes', 0.2)
+
+mikeshop.add_bikes(yellow, 5)
+mikeshop.add_bikes(red, 3)
+mikeshop.add_bikes(blue, 15)
+mikeshop.add_bikes(green, 1)
+mikeshop.add_bikes(black, 7)
+mikeshop.add_bikes(white, 10)
+
+Laura = Customer('Laura', 200)
+Michael = Customer('Michael', 500)
+Hannah = Customer('Hannah', 1000)
+mikecustomers = [Laura, Michael, Hannah]
+
+for person in mikecustomers:
+    person.affordable = []
+    print person.firstname, 'can buy:'
+    for bike in mikeshop.inventory:
+        price = bike.cost * (1 + mikeshop.margin)
+        if price <= person.budget:
+            print bike.model, "{:.2f}".format(price)
+            person.affordable.append(bike)
+    print person.affordable
+mikeshop.printstock()
+for person in mikecustomers:
+    person.buy(random.choice(person.affordable), mikeshop)
+    print person.own, 'Budget remaining:', "{:.2f}".format(person.budget)
+mikeshop.printstock()
+print 'Profit:', mikeshop.profit()
---
+++ bike_classes.py
@@ -0,0 +1,46 @@
+# Bicylce Industry Classes
+class Bike(object):
+    def __init__(self, model, weight, cost):
+        self.model = model
+        self.weight = weight
+        self.cost = cost
+class BikeShop(object):
+    inventory = {}
+    sold = {}
+    prof = 0
+    def __init__(self, name, margin):
+        self.name = name
+        self.margin = margin
+    def add_bikes(self, Bike, number):
+        self.inventory[Bike] = number
+        return self.inventory
+    def sell_bike(self, Bike):
+        price = Bike.cost * (1 + self.margin)
+        self.inventory[Bike] -= 1
+        self.sold[Bike] = price
+        return self.sold
+    def printstock(self):
+        print 'Current inventory for ' + str(self.name) + ':'
+        for bike in self.inventory:
+            print bike.model, self.inventory[bike]
+    def profit(self):
+        for Bike in self.sold:
+            self.prof += (self.sold[Bike] - Bike.cost)
+        return self.prof
+class Customer(object):
+    own = {}
+    def __init__(self, firstname, budget):
+        self.firstname = firstname
+        self.budget = budget
+    def buy(self, Bike, BikeShop):
+        price = Bike.cost * (1 + BikeShop.margin)
+        if price > self.budget:
+            print 'Cannot buy ' + str(Bike.model) + '! Too expensive.'
+        elif BikeShop.inventory[Bike] < 1:
+            print 'Cannot buy ' + str(Bike.model) + '! None left in stock.'
+        else:
+            self.own[Bike.model] = price
+            self.budget -= price
+            BikeShop.sell_bike(Bike)
+        return self.own
+    
